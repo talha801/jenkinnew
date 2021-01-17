9a01cf2dbe22f32f10e83522ee1378f0f0854dcd
talha801 <75091302+talha801@users.noreply.github.com>
2021-01-17 18:38:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "ken" {
-  bucket = "metalha-bucket"
+resource "aws_s3_bucket" "leona" {
+  bucket = "metalha005-bucket"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
-
-resource "aws_instance" "waqasec2" {
-  ami           = "ami-0e80a462ede03e653"
-  instance_type = "t2.micro"
-  
-  tags = {
-    Name        = "talhabhai"
-    }
 }
